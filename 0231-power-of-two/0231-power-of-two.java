736f47b93477cf704a5090d549823eb5aa626f01
Joker <77338424+TheJoker-Life@users.noreply.github.com>
2022-10-13 10:40:30 +0530

    Time: 2 ms (76.89%), Space: 41.6 MB (31.19%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,5 +1,12 @@
+// Recurssive Approach
+
 class Solution {
     public boolean isPowerOfTwo(int n) {
-     return n > 0 && (n & n-1) == 0;   
+        if(n == 1)
+            return true;
+        if(n %2 != 0 || n == 0)
+            return false;
+            
+        return isPowerOfTwo(n/2);
     }
 }
