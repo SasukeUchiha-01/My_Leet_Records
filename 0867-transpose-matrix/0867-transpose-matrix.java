cfb4e865875dccc7ca198f67f1be1e51c625fafc
Joker <77338424+TheJoker-Life@users.noreply.github.com>
2022-11-18 23:29:09 +0530

    Time: 1 ms (80.73%), Space: 48.8 MB (27.63%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -4,7 +4,7 @@ class Solution {
         int[][] ans = new int[matrix[0].length][matrix.length];
         
         for(int i = 0; i < matrix.length; i++){
-            for(int j = 0; j < matrix[i].length; j++){
+            for(int j = 0; j < matrix[0].length; j++){
                 ans[j][i] = matrix[i][j];
             }
         }
